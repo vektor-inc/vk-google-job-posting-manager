4c3022811e30864a5e0eeea71d51e59a7d92ec99
kurudrive <kurudrive@gmail.com>
2020-03-18 03:35:07 +0900

    [ Change version ] 1.1.4

--- vk-google-job-posting-manager.php
+++ vk-google-job-posting-manager.php
@@ -7,7 +7,7 @@
  * Author URI:      https://www.vektor-inc.co.jp
  * Text Domain:     vk-google-job-posting-manager
  * Domain Path:     /languages
- * Version:         1.1.2
+ * Version:         1.1.4
  *
  * @package         Vk_Google_Job_Posting_Manager
  */
