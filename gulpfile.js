bf766cb945e144dcc6ae5266da833ba46119dd17
drill-lancer <rickaddison7634@gmail.com>
2021-08-11 13:49:11 +0900

    fix; build

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,45 @@
-const gulp = require('gulp');
-const replace = require('gulp-replace');
+const gulp = require( 'gulp' );
+const replace = require( 'gulp-replace' );
 
 // replace_text_domain ////////////////////////////////////////////////
-gulp.task('replace_text_domain', function () {
-		return gulp.src(['./inc/custom-field-builder/package/*'])
-				.pipe(replace('custom_field_builder_textdomain', 'vk-google-job-posting-manager'))
-				.pipe(gulp.dest('./inc/custom-field-builder/package/'));
-});
+gulp.task( 'replace_text_domain', function () {
+	return gulp
+		.src( [ './inc/custom-field-builder/package/*' ] )
+		.pipe(
+			replace(
+				'custom_field_builder_textdomain',
+				'vk-google-job-posting-manager'
+			)
+		)
+		.pipe( gulp.dest( './inc/custom-field-builder/package/' ) );
+} );
 
 // copy dist ////////////////////////////////////////////////
 
-gulp.task('dist', function() {
-    return gulp.src(
-        [
-			'./**.php',
-			'./**.txt',
-			'./**.png',
-			'./**.jpg',
-			'./**.md',
-			'./assets/**',
-			'./blocks/**',
-			'./inc/**',
-			'./vendor/**',
-			'./languages/**',
-			"!./.distignore",
-			"!./.gitignore",
-			"!./Gruntfile.js",
-			"!./gulpfile.js",
-			"!./**.yml",
-			"!./**.json",
-			"!./**.dist",
-			"!./**.config.js",
-		],
-            { base: './' }
-	)
+gulp.task( 'dist', function () {
+	return gulp
+		.src(
+			[
+				'./**.php',
+				'./**.txt',
+				'./**.png',
+				'./**.jpg',
+				'./**.md',
+				'./assets/**',
+				'./blocks/**',
+				'./inc/**',
+				'./vendor/**',
+				'./languages/**',
+				'!./.distignore',
+				'!./.gitignore',
+				'!./Gruntfile.js',
+				'!./gulpfile.js',
+				'!./**.yml',
+				'!./**.json',
+				'!./**.dist',
+				'!./**.config.js',
+			],
+			{ base: './' }
+		)
 		.pipe( gulp.dest( 'dist' ) ); // distディレクトリに出力
 } );
