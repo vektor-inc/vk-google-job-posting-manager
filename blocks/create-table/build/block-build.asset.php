74be6ebddfe3b34591778ccadd691896cb3174f3
drill-lancer <rickaddison7634@gmail.com>
2025-12-04 09:41:50 +0900

    update: custom field builder

--- blocks/create-table/build/block-build.asset.php
+++ blocks/create-table/build/block-build.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'e13a2bed4ec490bbacc1');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'e646f025f5a90873c15c');
